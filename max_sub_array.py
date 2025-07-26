1147b69d1fcb2ec99c98bcc2cd5351b70cfc066b
BTR <thilakraj1301@gmail.com>
2025-07-25 18:48:05 -0700

    Create max_sub_array.py

---
+++ max_sub_array.py
@@ -0,0 +1,11 @@
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+def maxSubArray(nums):
+    if not nums:
+        return 0
+    max_sum = current_sum = nums[0]
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+    return max_sum
